27ff255d0a1334f91d668b81e05009b17a42a205
Allan Gilbert Rizza <allanrizza@gmail.com>
2025-08-26 23:02:54 -0300

    fixing bit counting

--- src/katas/kyu_6/bit_counting.rs
+++ src/katas/kyu_6/bit_counting.rs
@@ -4,11 +4,11 @@ fn count_bits(n: i64) -> u32 {
     let mut count: u32 = 0;
 
     while num > 0  {
-        i = n % 2;
+        i = num % 2;
         if i > 0 {
             count += 1;
         }
-        num = n / 2;
+        num = num / 2;
     }
 
     return count;
